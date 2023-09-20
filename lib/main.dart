6028ea26b6a1fd4f6785e292a415fc8848c12363
sohel7 <sohelju1983@gmail.com>
2023-09-20 23:35:03 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.amber),
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
+  }
+
+}
+
+class HomePage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+return Scaffold(
+  appBar: AppBar(
+   title: Text('My App'),
+  ),
+  body: Container(
+    width: double.infinity,
+    height: 300,
+    color: Colors.red,
+    alignment: Alignment.center,
+
+    child: AspectRatio(
+      aspectRatio: 16/9,
+      child: Container(
+        color: Colors.green,
+      ),
+    ),
+  ),
+);
+  }
+
+}
