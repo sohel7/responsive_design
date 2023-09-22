319dd38225e28445dc786db378aa7c41bce1742a
sohel7 <sohelocp@gmail.com>
2023-09-22 12:11:30 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,13 @@ body: Column(
   mainAxisAlignment: MainAxisAlignment.start,
   children: [
     Expanded(
-      flex: 1, // puro screen er 1+3+1  er 1 vag dokhol korbe
+      flex: 1, // puro screen er 1+3+1  er 1 vag dokhol korbe OR 100 vager 25 vag hisabeo dekhate pari jemon flex 25
         child: Container(color: Colors.blue,)),
     Expanded(
-      flex: 3, // puro screen er 1+3+2  er 3 vag dokhol korbe
+      flex: 3, // puro screen er 1+3+2  er 3 vag dokhol korbe OR 100 vager 75 vag hisabeo dekhate pari jemon flex 75
         child: Container(color: Colors.red,)),
     Expanded(
-      flex: 2, // // puro screen er 1+3+2  er 2 vag dokhol korbe
+      flex: 2, // // puro screen er 1+3+2  er 2 vag dokhol korbe OR 100 vager 75 vag hisabeo dekhate pari jemon flex 50
         child: Container(color: Colors.yellow,)),
 
   ],
